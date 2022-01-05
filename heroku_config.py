d7895e228ec280aef941db8427b4716de52582e7
Pranav24205 <96657174+Pranav24205@users.noreply.github.com>
2022-01-05 13:51:35 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -29,8 +29,8 @@ class Var(object):
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     #dont Kang this 
     botnickname = os.environ.get("BOT_NICK_NAME", None)
-    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", 5039520811:AAHnjUTBahKcc0uKKFjOHnQA5wtgp1GD-z4)
-    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", @SuperBotestvps_bot)
+    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", "5039520811:AAHnjUTBahKcc0uKKFjOHnQA5wtgp1GD-z4")
+    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", "@SuperBotestvps_bot")
     DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
     PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", None)
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
